779de85b5768741d4c0a7449e6bb7393dbc08b3c
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-27 21:21:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   // const newDriver = { ...driver};
   // newDriver[key]=value;
   // return newDriver;
-  return Object.assign(driver, { [key]: value });
+  return Object.assign({},driver, { [key]: value });
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
